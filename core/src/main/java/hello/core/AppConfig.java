82b142cbc6610003bf3a5d5efb259c3dd1c79d1d
WonJun Yeo <rbgh004@gmail.com>
2022-10-15 16:38:11 +0900

    <feat> add Test for getBean

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
     // 구현
     @Bean
     public MemberRepository memberRepository() {
