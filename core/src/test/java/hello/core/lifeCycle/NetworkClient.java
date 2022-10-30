d936e2cfa2167f199747225754bf5a815c3eacc5
WonJun Yeo <rbgh004@gmail.com>
2022-10-30 20:53:16 +0900

    <fix> modify spring bean life cycle method to annotation

--- core/src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifeCycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     //
     private String url;
@@ -29,6 +32,7 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         //
         System.out.println("init");
@@ -36,6 +40,7 @@ public class NetworkClient {
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("close");
         disconnect();
