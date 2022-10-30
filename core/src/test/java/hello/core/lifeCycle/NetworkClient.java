a69c7a84ac14cf2254d802a1322eedc6c6f904b7
WonJun Yeo <rbgh004@gmail.com>
2022-10-30 20:47:45 +0900

    <fix> spring bean life cycle method modify interface to initMtthod and destroyMethod

--- core/src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifeCycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     //
     private String url;
 
@@ -32,16 +29,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
         //
-        System.out.println("afterPropertiesSet");
+        System.out.println("init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
+        System.out.println("close");
         disconnect();
     }
 }
